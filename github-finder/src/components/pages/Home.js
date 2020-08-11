9f3990bf16b5a08973ba19ec57a3e02af89aae18
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-11 13:53:20 -0700

    deployment ready

--- github-finder/src/components/pages/Home.js
+++ github-finder/src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import Search from '../users/Search';
 import Users from '../users/Users';
-import User from '../users/User';
 
 const Home = () => (
 	<Fragment>
