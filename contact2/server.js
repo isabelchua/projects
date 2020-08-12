4e0a5da8af6ddcf20d2c0f3c20b6bab46ea94713
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-11 17:53:52 -0700

    edit

---
+++ contact2/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+	res.json({ msg: 'Welcome to the ContactKeeper API' })
+);
+
+//define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
